d3159dde1f479961a752a70a25db5361f81e763e
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-20 20:19:10 +0300

    Add project files.

---
+++ lab5ex6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va numara vocalele dintr-un sir de caractere citit de la tastatura.*/
+
+
+            Console.WriteLine("Introduceti sirul de caractere: ");
+            string sirCaractere = Console.ReadLine();
+
+            int contorVocale = 0;
+
+            for (int i = 0; i < sirCaractere.Length; i++)
+            {
+
+                if (sirCaractere[i] == 'a' || sirCaractere[i] == 'e' || sirCaractere[i] == 'i' ||
+                   sirCaractere[i] == 'o' || sirCaractere[i] == 'u')
+
+                {
+                    contorVocale++;
+
+                }
+            }
+
+            Console.WriteLine($"Sirul introdus are {contorVocale} vocale. ");
+        }
+     } 
+
+}
+
